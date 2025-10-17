ccdb24fc880c6daa89d37f9823bb7265419f1b45
sasori-256 <Sasori.0274@gmail.com>
2025-10-17 14:08:00 +0900

    update: add the 2nd class assignments

--- 3/ButtonFrame.java
+++ 3/ButtonFrame.java
@@ -2,15 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 class ButtonFrame extends JFrame {
-    public ButtonFrame(){
-      this.setSize(300,200);
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      JButton b = new JButton("OK");
-      this.add(b,BorderLayout.SOUTH);
-      this.setVisible(true);
-    }
+  public ButtonFrame() {
+    this.setSize(300, 200);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JButton b1 = new JButton("NORTH");
+    JButton b2 = new JButton("CENTER");
+    JButton b3 = new JButton("WEST");
+    JButton b4 = new JButton("EAST");
+    JButton b5 = new JButton("SOUTH");
+    this.add(b1, BorderLayout.NORTH);
+    this.add(b2, BorderLayout.CENTER);
+    this.add(b3, BorderLayout.WEST);
+    this.add(b4, BorderLayout.EAST);
+    this.add(b5, BorderLayout.SOUTH);
+    this.setVisible(true);
+  }
 
-    public static void main(String argv[]) {
-      new ButtonFrame();
-   }
+  public static void main(String argv[]) {
+    new ButtonFrame();
+  }
 }
--- 3/LabelFrame.java
+++ 3/LabelFrame.java
@@ -2,20 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 class LabelFrame extends JFrame {
-    public LabelFrame(){
-      this.setSize(300,200);
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      JButton b = new JButton("OK");
-      this.add(b,BorderLayout.SOUTH);
-      JLabel l = new JLabel("JLabel");
-      //JLabel l = new JLabel("<html><span style='font-size:36pt; color:blue; " 
-      //    +"background-color:yellow;'>JLabel</span></html>",JLabel.CENTER);
-      // JLabel l = new JLabel("<html><img src='file:lion.jpg' width=100 height=100></html>",JLabel.CENTER);
-      this.add(l,BorderLayout.CENTER);
-      this.setVisible(true);
-    }
+  public LabelFrame() {
+    this.setSize(400, 250);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JButton b = new JButton("ライオン!");
+    this.add(b, BorderLayout.SOUTH);
 
-    public static void main(String argv[]) {
-      new LabelFrame();
-   }
+    JLabel l1 = new JLabel("<html><img src='file:lion.jpg' width=100 height=100><br>こんにちは 🦁</html>",
+        JLabel.CENTER);
+    JLabel l2 = new JLabel(
+        "<html><span style='font-size:36pt; color:blue; background-color:yellow;'>ライオンだよ</span></html>",
+        JLabel.CENTER);
+    JLabel lWest = new JLabel("<html><span style='font-size:18pt; color:green;'>ライオン</span></html>",
+        JLabel.CENTER);
+    JLabel lEast = new JLabel("<html><span style='font-size:18pt; color:red;'>ライオン</span></html>", 
+        JLabel.CENTER);
+
+    this.add(l1, BorderLayout.NORTH);
+    this.add(l2, BorderLayout.CENTER);
+    this.add(lWest, BorderLayout.WEST);
+    this.add(lEast, BorderLayout.EAST);
+
+    this.setVisible(true);
+    this.setVisible(true);
+  }
+
+  public static void main(String argv[]) {
+    new LabelFrame();
+  }
 }
